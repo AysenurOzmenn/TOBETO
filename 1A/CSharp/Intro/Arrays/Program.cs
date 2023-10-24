69cf906bd906f62ab951763389be928eee9cbbb3
Ayşenur Özmen <142101336+AysenurOzmenn@users.noreply.github.com>
2023-10-24 23:37:08 +0300

    Initial commit

---
+++ 1A/CSharp/Intro/Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] students = new string[3] { "Engin", "Derin", "Salih"};
+        
+        string[] students2 = new string[3];
+        students[0] = "Engin";
+        students[1] = "Derin";
+        students[2] = "Salih";
+
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+
+        string[,] regions=new string[5, 3] 
+        {
+            { "A","B","C"},
+            { "D","E","F"},
+            { "G","H","I"},
+            { "J","K","L"},
+            { "M","N","O"},
+        };
+
+        for (int i = 0; i <= regions.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= regions.GetUpperBound(1); j++)
+            {
+                Console.WriteLine(regions[i, j]);
+            }
+            Console.WriteLine("*****************");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ 1A/CSharp/Intro/Intro/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    //***Bu projede başlangıç ve veri tipleri örnekleri yapıldı.***
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        double number5 = 10.4;
+        decimal number6 = 10;
+        char character = 'a';
+        string city = "Ankara";
+        bool   condition = false;
+        byte   number4 = 16;                     //8bit
+        short  number3 = 32767;                  //16bit
+        int    number1 = 2147483647;             //32bit
+        long   number2 = -9223372036854775807;   //64bit
+        var number7 = 10;
+        //number7 = 'A';
+        Console.WriteLine("Number1 is {0}", number1);
+        Console.WriteLine("Number2 is {0}", number2);
+        Console.WriteLine("Number3 is {0}", number3);
+        Console.WriteLine("Number4 is {0}", number4);
+        Console.WriteLine("Number5 is {0}", number5);
+        Console.WriteLine("Number6 is {0}", number6);
+        Console.WriteLine("Character is {0}", character);
+        Console.WriteLine((int)Days.Friday);
+        Console.ReadLine();
+    }
+}
+enum Days
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
